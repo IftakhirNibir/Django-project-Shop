5d61100b6f6927bff018cdfd02fa44fa7ca04ed1
Nibir19 <109109843+Nibir19@users.noreply.github.com>
2023-09-18 18:04:25 +0600

    final commit

--- market/core/views.py
+++ market/core/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render,redirect
 from item.models import Category, Item
+from core.models import Review
 from .forms import SignupForm, FeedbackForm
 # Create your views here.
 
 def index(request):
-    items = Item.objects.filter(is_sold=False)[0:6] #because we don't want product that is sold
+    items = Item.objects.filter(is_sold=False)[0:8] #because we don't want product that is sold
     categories = Category.objects.all()
+    reviews = Review.objects.filter(is_viewed=True)
 
     return render(request, 'index.html', {
         'categories': categories,
         'items': items,
+        'reviews': reviews,
     })
 
 def review(request):
